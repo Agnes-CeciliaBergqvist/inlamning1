901377e1e349211b4f528a752b2def4fe1099227
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-02 11:33:01 +0200

    First commit, header and footer done

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+function get_files() {
+    wp_enqueue_script('jQuery', get_theme_file_uri('/js/jquery.js'), 'jquery', '1.0', false);
+    wp_enqueue_script('scripts', get_theme_file_uri('/js/script.js'), 'jquery', '1.0', true);
+    wp_enqueue_style('Bootstrap', get_theme_file_uri('/css/bootstrap.css'));
+    wp_enqueue_style('Font-Awesome', get_theme_file_uri('/css/font-awesome.css'));
+    wp_enqueue_style('css-style', get_theme_file_uri('/css/style.css'));
+    
+
+}
+
+add_action('wp_enqueue_scripts', 'get_files'); 
+
