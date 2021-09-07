226578d9c433d32e442619dbaaba5aaa66e4e754
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-07 09:40:14 +0200

    Added wp-menu

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php 
 
 function get_files() {
-    wp_enqueue_script('jQuery', get_theme_file_uri('/js/jquery.js'), 'jquery', '1.0', false);
+    // jQuery behövs inte ??
+    // wp_enqueue_script('jQuery', get_theme_file_uri('/js/jquery.js'), 'jquery', '1.0', false);
     wp_enqueue_script('scripts', get_theme_file_uri('/js/script.js'), 'jquery', '1.0', true);
     wp_enqueue_style('Bootstrap', get_theme_file_uri('/css/bootstrap.css'));
     wp_enqueue_style('Font-Awesome', get_theme_file_uri('/css/font-awesome.css'));
